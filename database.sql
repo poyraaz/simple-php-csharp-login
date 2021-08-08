f8188537a0d7bfd2a3a2a754576bb6216e4d070c
Poyraz <67389553+poyraaz@users.noreply.github.com>
2021-08-09 01:35:39 +0300

    vecho

---
+++ database.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 09 Ağu 2021, 00:34:57
+-- Sunucu sürümü: 10.4.20-MariaDB
+-- PHP Sürümü: 7.3.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `vecho`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `users`
+--
+
+CREATE TABLE `users` (
+  `uuid` varchar(255) NOT NULL,
+  `name` varchar(16) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `credit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `users`
+--
+
+INSERT INTO `users` (`uuid`, `name`, `password`, `credit`) VALUES
+('864c885b-a723-4527-822a-d106c04a1f43', 'Ahmetflix', 'test', 1),
+('864c885b-a723-4527-822a-d106c04a1f43', 'Ahmetflix', 'test', 1),
+('test', 'Poyraz', 'asdasd', 40);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
