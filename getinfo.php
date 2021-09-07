d20d913dcc4e4636fde9aba0778c061917d20ff4
Poyraz <67389553+poyraaz@users.noreply.github.com>
2021-09-07 22:39:04 +0300

    Update getinfo.php

--- getinfo.php
+++ getinfo.php
@@ -3,9 +3,11 @@ header("Content-Type: application/json");
 $servername = "localhost"; // Sunucu
 $username = "root"; // Kullanıcı Adı
 $password = ""; // Şifre
+$db = "database"; // Database İsmi
+
 try
 {
-    $conn = new PDO("mysql:host=$servername;dbname=dbName", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname='{$db}'", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e)
