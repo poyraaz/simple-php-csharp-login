25f0c09c68973826a0fe68fdf820ee00e3ed2d28
Poyraz <67389553+poyraaz@users.noreply.github.com>
2021-09-28 13:22:01 +0300

    Update getinfo.php

--- getinfo.php
+++ getinfo.php
@@ -7,7 +7,7 @@ $db = "database"; // Database İsmi
 
 try
 {
-    $conn = new PDO("mysql:host=$servername;dbname='{$db}'", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e)
